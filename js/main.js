326761b758439d0e439386fc1d16c9ca54a644a4
adrielsanchez2269 <adrielsanchez2269@gmail.com>
2022-11-12 16:12:38 -0500

    sfd

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
         return false;
     });
 
